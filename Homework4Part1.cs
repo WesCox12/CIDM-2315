1439bacdb7bda20df5c2522f33048281958282af
WesCox12 <112525173+WesCox12@users.noreply.github.com>
2022-09-21 21:41:41 -0500

    Homework 4

---
+++ Homework4Part1.cs
@@ -0,0 +1,40 @@
+﻿namespace Homework4;
+class Program
+{
+    
+static int largest(int a, int b){
+
+if(a>b){
+
+return a;
+}
+else if(b>a){
+
+return b;
+}
+else{
+
+return -1;
+}
+
+}
+
+    static void Main(string[] args)
+    {
+        Console.Write("First Number = ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Second Number = ");
+        int b = int.Parse(Console.ReadLine());
+
+
+
+int lar = largest( a, b);
+
+
+Console.WriteLine("a = {0}; b = {1}",a,b);
+Console.WriteLine("The largest number is : "+lar);
+
+}
+    }
+
---
+++ Homework4Part2.cs
@@ -0,0 +1,35 @@
+﻿namespace HW4PT2;
+class Program
+{
+    static void pattern(int N,string shape){
+    int i,j,k;
+    if(shape=="left"){
+       for(i=1;i<=N;i++)
+       {
+        for(j=1;j<=i;j++)
+    	Console.Write("*");
+    	Console.Write("\n"); 
+       }
+      }
+    else if(shape=="right"){
+     for (i = 1; i <= N; i++) 
+         {  
+            for (j = 1; j <= N-i; j++)  
+            {Console.Write(" ");}  
+            for (k = 1; k <= i; k++)  
+            {  Console.Write("*");}  
+            Console.WriteLine(""); 
+            }  
+        Console.ReadLine();
+        }
+    }
+    public static void Main() {
+   int N; 
+   string shape;
+   Console.Write("Input N: ");
+   N= Convert.ToInt32(Console.ReadLine());
+   Console.Write("Input shape: ");
+   shape= Console.ReadLine();
+   Console.Write("N is: "+N+"; shape is "+shape+"\n");
+   pattern(N,shape);
+}}

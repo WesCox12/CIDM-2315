c56eaed13a8eafa2e26962e85a8abe5b94598e0b
WesCox12 <112525173+WesCox12@users.noreply.github.com>
2022-09-28 21:41:20 -0500

    Homework 5 Part 1

---
+++ Homework5Part1.cs
@@ -0,0 +1,23 @@
+﻿namespace Week5HW;
+class Program
+{
+public static int largest(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+    static void Main(string[] args)
+    {
+         Console.Write("a = ");
+        int first = Convert.ToInt32(Console.ReadLine());
+        Console.Write("b = ");
+        int second = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The largest number is: " + largest(first, second));
+    }
+}

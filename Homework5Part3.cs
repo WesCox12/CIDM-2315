ab0fb9e0b797a2acffe528db863408a7ce57a035
WesCox12 <112525173+WesCox12@users.noreply.github.com>
2022-09-28 21:53:59 -0500

    Homework 5 Part 3

---
+++ Homework5Part3.cs
@@ -0,0 +1,51 @@
+﻿namespace Week5HW;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        makeAccount();
+Console.ReadKey(true);
+    }
+    public static bool checkAge(int birth_Year)
+{
+int age=2022-birth_Year;
+if(age>=18)
+return true;
+else
+return false;
+}
+public static void makeAccount()
+{
+String userName,password,passwordAgain;
+int birthYear;
+
+Console.WriteLine("Enter Your Username:");
+
+userName=Console.ReadLine();
+
+Console.WriteLine("Enter Your Password:");
+
+password=Console.ReadLine();
+
+Console.WriteLine("Enter Your Password Again:");
+
+passwordAgain=Console.ReadLine();
+
+Console.WriteLine("Enter Your Birth Year:");
+
+birthYear=Convert.ToInt32(Console.ReadLine());
+
+if(checkAge(birthYear))
+{
+if(password==passwordAgain)
+Console.WriteLine("Account is created successfully");
+else
+Console.WriteLine("Wrong password");
+}
+
+else
+Console.WriteLine("Could not create an account.");
+
+}
+}

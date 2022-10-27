d1aed9c6e16a1bca3bfcea4706d9965643afb893
WesCox12 <112525173+WesCox12@users.noreply.github.com>
2022-10-26 21:12:50 -0500

    Upload Homework8 Part 1

---
+++ Homework8Part1.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework8;
+class Program
+{
+    public static void ArraySum(int[] int_array){
+        int sum = 0;
+        foreach (int item in int_array) {
+            sum += item;
+        }
+        Console.WriteLine($"The Sum of int-array is: {sum}");
+    }
+    
+    public static void Main(string[] args)
+    {
+        int[] array = { 11, 23, 31, 42, 53 };
+        ArraySum(array);
+        
+    }
+    
+    
+}
